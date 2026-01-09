5d9e8a595b62f3dab9648247f3941b011bf15612
Nguyen Tran <me@mac.local>
2026-01-09 14:50:28 -0600

    remove shell config changes and properly update the package list

--- install.py
+++ install.py
@@ -1,6 +1,5 @@
 import os
 import shutil
-import platform
 if os.path.isdir("_internal"):
     shutil.rmtree("_internal")
     os.remove("cepm")
@@ -11,12 +10,3 @@ os.chdir("../..")
 shutil.rmtree("build")
 shutil.rmtree("dist")
 os.remove("cepm.spec")
-if platform.system() == 'Linux':
-    os.system(f"echo 'export PATH=\"{os.path.dirname(os.path.realpath(__file__)).split("/_internal")[0]}:$PATH\"' > ~/.bashrc")
-    os.system(f"echo 'export PATH=\"{os.path.dirname(os.path.realpath(__file__)).split("_internal")[0]}/bin:$PATH\"' >> ~/.bashrc")
-    os.system(f"source ~/.bashrc")
-elif platform.system() == 'Darwin':
-    os.system(f"echo 'export PATH=\"{os.path.dirname(os.path.realpath(__file__)).split("/_internal")[0]}:$PATH\"' > ~/.zshrc")
-    os.system(f"echo 'export PATH=\"{os.path.dirname(os.path.realpath(__file__)).split("_internal")[0]}/bin:$PATH\"' >> ~/.zshrc")
-    os.system(f"echo 'export PATH=\"/opt/homebrew/bin:$PATH\"' >> ~/.zshrc")
-    os.system(f"source ~/.zshrc")
