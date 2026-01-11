00f3334cae641ea3b766f3cd3ce02a0f96b3b9c5
Nguyen Tran <me@mac.local>
2026-01-11 08:02:14 -0600

    add fast version of draw-engine

--- scripts/draw-engine.py
+++ scripts/draw-engine.py
@@ -1,8 +1,16 @@
 import os
 import subprocess
+import shutil
 os.chdir("src/draw-engine")
 subprocess.run("python3 install.py".split(), input=input("Engine Path: ").encode("utf-8"))
-if os.path.isdir("../../bin/draw-engine") == False:
-    os.mkdir("../../bin/draw-engine")
+if os.path.isdir("../../bin/draw-engine"):
+    shutil.rmtree("../../bin/draw-engine")
+os.mkdir("../../bin/draw-engine")
 os.rename("draw-engine", "../../bin/draw-engine/draw-engine")
 os.rename("_internal", "../../bin/draw-engine/_internal")
+subprocess.run("python3 install_fast.py".split(), input=input("Engine Path: ").encode("utf-8"))
+if os.path.isdir("../../bin/drawmaster"):
+    shutil.rmtree("../../bin/drawmaster")
+os.mkdir("../../bin/drawmaster")
+os.rename("drawmaster", "../../bin/drawmaster/drawmaster")
+os.rename("_internal", "../../bin/drawmaster/_internal")
